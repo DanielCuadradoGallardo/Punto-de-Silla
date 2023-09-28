44ad2f90c9cc64c8fe2e03f60ea1527942daaf69
Daniel Cuadrado Gallardo <danielcuadradogallardo@cdpjosecabrera.es>
2023-09-28 09:52:38 +0200

    Creada clase PuntoDeSilla
    Reorganizado el ejercicio para ceñir la aplicación explícitamente al enunciado.

--- PuntoDeSilla/Program.cs
+++ PuntoDeSilla/Program.cs
@@ -16,18 +16,10 @@
                     Console.WriteLine("Introduce dimension n");
                     int n = int.Parse(Console.ReadLine());
 
-                    int[,] array = new int[m, n];
 
-                    for (int i = 0; i < m; i++)
-                    {
-                        for (int j = 0; j < n; j++)
-                        {
-                            Console.WriteLine("Introduce posicion[" + i + "][" + j + "]");
-                            array[i, j] = int.Parse(Console.ReadLine());
-                        }
-                    }
+                    var puntoDeSilla = new PuntoDeSilla(m, n);
+                    var results = puntoDeSilla.getPuntoDeSilla();
 
-                    var results = puntoDeSilla(array);
                     if (results.Item3 != false)
                     {
                         Console.WriteLine("El valor del primer punto de silla es: " + results.Item1 + " y se encuentra en la posicion " + results.Item2);
@@ -48,45 +40,77 @@
 
             } while (error == true);
         }
-        private static (int, string, bool) puntoDeSilla(int[,] array)
+
+        public class PuntoDeSilla
         {
-            int puntoDeSilla = 0;
-            bool esMenorEnSuFila = true;
-            bool esMayorEnSuColumna = true;
-            string posicionPuntoDeSilla = "[][]";
+            int m;
+            int n;
+            int[,] array;
 
-            for (int i = 0; i < array.GetLength(0); i++)
+            public PuntoDeSilla(int m, int n)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    esMenorEnSuFila = true;
-                    esMayorEnSuColumna = true;
-                    for (int z = 0; z < array.GetLength(1); z++)
-                    {
-                        if (array[i, j] >= array[i, z])
-                        {
-                            esMenorEnSuFila = false;
-                        }
-                    }
-                    for (int z = 0; z < array.GetLength(0); z++)
-                    {
-                        if (array[i, j] <= array[z, j])
-                        {
-                            esMayorEnSuColumna = false;
-                        }
-                    }
-                    if (esMenorEnSuFila == true && esMayorEnSuColumna == true)
-                    {
-                        puntoDeSilla = array[i, j];
-                        posicionPuntoDeSilla = "[" + i + "][" + j + "]";
+                this.m = m;
+                this.n = n;
+                this.array = createArray();
+            }
 
-                        return (puntoDeSilla, posicionPuntoDeSilla, true);
+            private int[,] createArray()
+            {
+                int[,] array2 = new int[m, n];
+
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.WriteLine("Introduce posicion[" + i + "][" + j + "]");
+                        array2[i, j] = int.Parse(Console.ReadLine());
                     }
                 }
 
+                return array2;
             }
 
-            return (0, "", false);
+            public (int, string, bool) getPuntoDeSilla()
+            {
+                int puntoDeSilla = 0;
+                bool esMenorEnSuFila = true;
+                bool esMayorEnSuColumna = true;
+                string posicionPuntoDeSilla = "[][]";
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        esMenorEnSuFila = true;
+                        esMayorEnSuColumna = true;
+                        for (int z = 0; z < array.GetLength(1); z++)
+                        {
+                            if (array[i, j] > array[i, z])
+                            {
+                                esMenorEnSuFila = false;
+                            }
+                        }
+                        for (int z = 0; z < array.GetLength(0); z++)
+                        {
+                            if (array[i, j] < array[z, j])
+                            {
+                                esMayorEnSuColumna = false;
+                            }
+                        }
+                        if (esMenorEnSuFila == true && esMayorEnSuColumna == true)
+                        {
+                            puntoDeSilla = array[i, j];
+                            posicionPuntoDeSilla = "[" + i + "][" + j + "]";
+
+                            return (puntoDeSilla, posicionPuntoDeSilla, true);
+                        }
+                    }
+
+                }
+
+                return (0, "", false);
+            }
         }
+        
     }
 }
